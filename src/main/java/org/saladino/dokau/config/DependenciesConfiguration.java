9b170e73d3b8e4c0ebcfca89e8fe65c28fb9f9a5
MuryloSaladino <murylosaladino@gmail.com>
2024-07-29 11:08:23 -0300

    feat: utility beans

--- src/main/java/org/saladino/dokau/config/DependenciesConfiguration.java
+++ src/main/java/org/saladino/dokau/config/DependenciesConfiguration.java
@@ -1,7 +1,26 @@
 package org.saladino.dokau.config;
 
-import org.springframework.context.annotation.Configuration;
+import org.saladino.dokau.interfaces.JwtTokenManager;
+import org.saladino.dokau.services.JwtTokenManagerService;
+import org.saladino.dokau.utility.UserSession;
+import org.springframework.context.annotation.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.context.WebApplicationContext;
 
 @Configuration
 public class DependenciesConfiguration {
+
+    // UTILITY BEANS
+
+    @Bean @Scope
+    JwtTokenManager jwtTokenManager() { return new JwtTokenManagerService(); }
+
+    @Bean @Scope
+    BCryptPasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
+
+
+    // SESSION BEANS
+
+    @Bean @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public UserSession userSession() { return new UserSession(); }
 }
