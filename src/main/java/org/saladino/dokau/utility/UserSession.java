ad404512189b3c2d48628f4958911538b1f2a6be
MuryloSaladino <murylosaladino@gmail.com>
2024-07-29 11:13:45 -0300

    feat: user request session

---
+++ src/main/java/org/saladino/dokau/utility/UserSession.java
@@ -0,0 +1,11 @@
+package org.saladino.dokau.utility;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UserSession {
+
+    private Long id;
+    private Boolean admin;
+}
