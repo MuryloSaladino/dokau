4847f2ba5ff98879d06c6d1d8f375b4a2f87fb6e
MuryloSaladino <murylosaladino@gmail.com>
2024-08-01 11:13:20 -0300

    feat: user data population

---
+++ src/main/java/org/saladino/dokau/data/UsersDataLoader.java
@@ -0,0 +1,47 @@
+package org.saladino.dokau.data;
+
+import org.saladino.dokau.entities.UserDetailsEntity;
+import org.saladino.dokau.entities.UserEntity;
+import org.saladino.dokau.repositories.UserDetailsRepository;
+import org.saladino.dokau.repositories.UserEntityRepository;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+
+@Component
+public class UsersDataLoader implements ApplicationRunner {
+
+    private final BCryptPasswordEncoder encoder;
+    private final UserEntityRepository userRepository;
+    private final UserDetailsRepository userDetailsRepository;
+
+    public UsersDataLoader(
+            BCryptPasswordEncoder encoder,
+            UserEntityRepository userRepository,
+            UserDetailsRepository userDetailsRepository
+    ) {
+        this.encoder = encoder;
+        this.userRepository = userRepository;
+        this.userDetailsRepository = userDetailsRepository;
+    }
+
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        userRepository.deleteAll();
+        userDetailsRepository.deleteAll();
+
+        // ADMIN USER
+        userRepository.save(new UserEntity(
+                "admin",
+                encoder.encode("123456"),
+                "admin@mail.com",
+                true,
+                new UserDetailsEntity("Administrator", "", new Timestamp(1L))
+        ));
+    }
+}
