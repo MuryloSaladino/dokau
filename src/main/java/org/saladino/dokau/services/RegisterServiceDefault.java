69d6785c971e9c224db052fb86a24b808e4a3d52
MuryloSaladino <murylosaladino@gmail.com>
2024-08-07 11:16:45 -0300

    feat: register user

--- src/main/java/org/saladino/dokau/services/RegisterServiceDefault.java
+++ src/main/java/org/saladino/dokau/services/RegisterServiceDefault.java
@@ -1,49 +1,63 @@
 package org.saladino.dokau.services;
 
-import org.saladino.dokau.entities.RegisterEntity;
+import org.saladino.dokau.dto.access.RegisterPayload;
+import org.saladino.dokau.dto.users.UserEntityResponse;
+import org.saladino.dokau.entities.PreRegisterEntity;
 import org.saladino.dokau.exceptions.NotFoundException;
 import org.saladino.dokau.interfaces.EmailSender;
 import org.saladino.dokau.interfaces.JwtTokenManager;
 import org.saladino.dokau.interfaces.RegisterService;
 import org.saladino.dokau.pages.HTMLPages;
-import org.saladino.dokau.repositories.RegisterRepository;
+import org.saladino.dokau.repositories.PreRegisterRepository;
+import org.saladino.dokau.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class RegisterServiceDefault implements RegisterService {
 
-    private final RegisterRepository registerRepository;
+    private final PreRegisterRepository preRegisterRepository;
     private final JwtTokenManager jwtTokenManager;
     private final EmailSender emailSender;
+    private final UserRepository userRepository;
 
     public RegisterServiceDefault(
-            final RegisterRepository registerRepository,
+            final PreRegisterRepository preRegisterRepository,
             final JwtTokenManager jwtTokenManager,
-            final EmailSender emailSender
+            final EmailSender emailSender,
+            final UserRepository userRepository
     ) {
-        this.registerRepository = registerRepository;
+        this.preRegisterRepository = preRegisterRepository;
         this.jwtTokenManager = jwtTokenManager;
         this.emailSender = emailSender;
+        this.userRepository = userRepository;
     }
 
 
     @Override
-    public void register(String email) {
-        registerRepository.save(new RegisterEntity(email));
+    public void preRegister(String email) {
+        preRegisterRepository.save(new PreRegisterEntity(email));
     }
 
     @Override
     public void sendConfirmationEmail(String email) {
-        var query = registerRepository.findByEmail(email);
+        var query = preRegisterRepository.findByEmail(email);
         if(query.isEmpty()) throw new NotFoundException("That email has not been registered");
 
-        Map<String, Object> claims = new HashMap<>();
-        claims.put("email", email);
-        String token = jwtTokenManager.buildToken(claims, "register", query.get().getId());
+        String token = jwtTokenManager.buildToken(new HashMap<>(), email, query.get().getId());
 
         emailSender.sendEmail(email, "Confirmation email", HTMLPages.confirmEmailPage(token));
+    }
+
+    @Override
+    public UserEntityResponse register(RegisterPayload payload, String token) {
+        var claims = jwtTokenManager.extractClaims(token);
+        var query = preRegisterRepository.findByEmail(claims.getSubject());
+        if(query.isEmpty()) throw new NotFoundException("Pre register not found in our database");
+
+        preRegisterRepository.deleteByEmail(claims.getSubject());
+
+        return new UserEntityResponse( userRepository.save( payload.getUser(claims.getSubject()) ) );
     }
 }
