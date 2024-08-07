c81b0f869668ba0ef386a43d9afb654e4470a61d
MuryloSaladino <murylosaladino@gmail.com>
2024-08-07 10:42:54 -0300

    feat: pre register data

---
+++ src/main/java/org/saladino/dokau/entities/RegisterEntity.java
@@ -0,0 +1,13 @@
+package org.saladino.dokau.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity @Table @NoArgsConstructor @AllArgsConstructor
+public class RegisterEntity extends BaseEntity {
+    @Column(unique = true)
+    private String email;
+}
