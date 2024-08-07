bc7b1fe7325366493ba277c950b206caa9d411a9
MuryloSaladino <murylosaladino@gmail.com>
2024-08-07 11:16:39 -0300

    feat: register user

--- src/main/java/org/saladino/dokau/interfaces/RegisterService.java
+++ src/main/java/org/saladino/dokau/interfaces/RegisterService.java
@@ -1,6 +1,10 @@
 package org.saladino.dokau.interfaces;
 
+import org.saladino.dokau.dto.access.RegisterPayload;
+import org.saladino.dokau.dto.users.UserEntityResponse;
+
 public interface RegisterService {
-    void register(String email);
+    void preRegister(String email);
     void sendConfirmationEmail(String email);
+    UserEntityResponse register(RegisterPayload payload, String email);
 }
